70cce8dc117c20b6d562ea34fa767313df24f734
Sascha Dewald <sascha.dewald@gmail.com>
2025-09-12 13:49:42 +0200

    change name

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "builder-space-dev"
+  default     = "funda"
 }
 
 variable "cluster_version" {
